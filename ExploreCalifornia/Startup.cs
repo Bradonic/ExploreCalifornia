0c316cbeb887b72b9ac8feff6becb810884c13e1
Bradley van der Westhuizen <bradleyvanderwesthuizen7@gmail.com>
2021-05-14 17:57:41 +0200

    first commit

---
+++ ExploreCalifornia/Startup.cs
@@ -0,0 +1,80 @@
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia
+{
+    public class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<FormattingService>();
+
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                DevelopmentExceptions = configuration.GetValue<bool>("FeatureToggles:DevelopmentExceptions")
+            });
+
+            services.AddDbContext<BlogDataContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("BlogDataContext");
+                options.UseSqlServer(connectionString);
+            });
+
+            services.AddDbContext<IdentityDataContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("IdentityDataContext");
+                options.UseSqlServer(connectionString);
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDataContext>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, FeatureToggles features)
+        {
+            app.UseExceptionHandler("/error.html");
+
+            if (features.DevelopmentExceptions)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("ERROR!");
+
+                await next();
+            });
+
+            app.UseIdentity();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
+            app.UseFileServer();
+        }
+    }
+}
